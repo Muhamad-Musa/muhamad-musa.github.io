30dc1cf94bafdc5086fd32cb622c693854b89b54
MuhamadBarzani <barzandolamari3@gmail.com>
2025-10-03 18:35:44 +0300

    Update stylesheet references in index.html and projects.html for consistent styling

--- docs/.vuepress/.temp/internal/themeData.js
+++ docs/.vuepress/.temp/internal/themeData.js
@@ -1,14 +1,1 @@
 export const themeData = JSON.parse("{\"navbar\":[{\"text\":\"Home\",\"link\":\"/\"},{\"text\":\"Projects\",\"link\":\"/projects\"},{\"text\":\"About\",\"link\":\"/#about-me\"},{\"text\":\"Skills\",\"link\":\"/#skills-interests\"},{\"text\":\"Contact\",\"link\":\"/#contact\"}],\"sidebar\":false,\"editLink\":false,\"lastUpdated\":false,\"contributors\":false,\"footer\":\"© 2025 Muhamad Barzani. All rights reserved.\",\"displayFooter\":true,\"home\":false,\"locales\":{\"/\":{\"selectLanguageName\":\"English\"}},\"colorMode\":\"auto\",\"colorModeSwitch\":true,\"logo\":null,\"repo\":null,\"selectLanguageText\":\"Languages\",\"selectLanguageAriaLabel\":\"Select language\",\"sidebarDepth\":2,\"editLinkText\":\"Edit this page\",\"contributorsText\":\"Contributors\",\"notFound\":[\"There's nothing here.\",\"How did we get here?\",\"That's a Four-Oh-Four.\",\"Looks like we've got some broken links.\"],\"backToHome\":\"Take me home\",\"openInNewWindow\":\"open in new window\",\"toggleColorMode\":\"toggle color mode\",\"toggleSidebar\":\"toggle sidebar\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateThemeData) {
-    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ themeData }) => {
-    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
-  })
-}
